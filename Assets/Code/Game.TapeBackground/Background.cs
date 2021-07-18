fc74941da7caf0b3682292887c7a6fc55e2608cc
AlekTananykin <tananykin@mail.ru>
2021-07-18 08:26:19 +0300

    moving is ready

--- Assets/Code/Game.TapeBackground/Background.cs
+++ Assets/Code/Game.TapeBackground/Background.cs
@@ -15,7 +15,7 @@ public class Background : MonoBehaviour
 
     public void Move(float value)
     {
-        transform.position += Vector3.right * _relativeSpeedRate;
+        transform.position += Vector3.right * _relativeSpeedRate * value;
         Vector3 position = transform.position;
 
         if (position.x <= _leftBorder)
@@ -23,7 +23,7 @@ public class Background : MonoBehaviour
                 _rightBorder - (_leftBorder - position.x), position.y, position.z);
         else if (transform.position.x >= _rightBorder)
             transform.position = new Vector3(
-                _leftBorder + (_rightBorder - position.x), position.y, position.z);
+                _leftBorder + (position.x - _rightBorder), position.y, position.z);
 
     }
 }
